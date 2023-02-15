ebb03c408865b286892476176663701bb7b17b06
Hristiyan Dodov <h.dodov@gmail.com>
2023-02-15 09:00:12 +0200

    Add print chars solution

---
+++ 2023-02-13/print-chars.ts
@@ -0,0 +1,11 @@
+/**
+ * FEB. 13, 2023, 1:12 A.M.
+ * https://buttondown.email/cassidoo/archive/if-you-dont-live-your-life-then-who-will-rihanna/
+ * https://twitter.com/cassidoo/status/1625030313318445062
+ * https://twitter.com/yandodov/status/1625753714660192257
+ *
+ * Print the numbers 0 through 100 without using the characters 1, 2, 3, 4, 5,
+ * 6, 7, 8, or 9 in your code. Get creative!
+ */
+
+for (let i = 0; i <= "d".charCodeAt(0); i++) console.log(i);
